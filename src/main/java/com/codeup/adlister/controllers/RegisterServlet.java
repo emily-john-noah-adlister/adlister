1caab307e981ef564485ac6fadf43ac934c8fb19
John Wallace <jnwywe@gmail.com>
2018-06-13 09:48:31 -0500

    pre merge push

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -49,4 +49,5 @@ public class RegisterServlet extends HttpServlet {
         DaoFactory.getUsersDao().insert(new User(username, email, password));
         response.sendRedirect("/login");
     }
+
 }
